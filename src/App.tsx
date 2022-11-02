96674672b202b18b92da7e6480c49c99ee6df1ae
OlgaGrishchenko <olg.grishchenko@gmail.com>
2022-11-02 18:52:26 +0300

    added components card, cardslist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./index.css";
 
 //@ts-ignore
 import styles from "./App.module.css";
-import ResetPassword from "./Pages/ResetPassword";
+import CardsList from "./Components/CardsList";
+import { CardType } from "./Constants/@types";
+
+const MOCK_CARD = {
+    id: 0,
+    image: "https://pibig.info/uploads/posts/2022-03/1648204988_5-pibig-info-p-kvadratnaya-priroda-priroda-krasivo-foto-6.jpg",
+    text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
+    date: "2022-11-01",
+    lesson_num: 0,
+    title: "Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    author: 0,
+};
+
+const MOCK_CARDS_LIST = [
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+    MOCK_CARD,
+];
 
 const App = () => {
+    const [cardsList, setCardsList] = useState<CardType[] | null>(null);
+
+    useEffect(() => {
+        setCardsList(MOCK_CARDS_LIST);
+    }, []);
+
     return (
         <div className={styles.container}>
-           <ResetPassword />
+            <CardsList cardsList={MOCK_CARDS_LIST} />
         </div>
     );
 };
---
+++ src/Assets/index.ts
@@ -0,0 +1,6 @@
+export * from "./icons/BurgerClosedIcon";
+export * from "./icons/CloseIcon";
+export { LikeIcon } from "./icons/LikeIcon";
+export { DislikeIcon } from "./icons/DislikeIcon";
+export { BookmarkIcon } from "./icons/BookmarkIcon";
+export { MoreIcon } from "./icons/MoreIcon";
