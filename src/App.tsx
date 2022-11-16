54779a5c16e732c58e78dba739954a25fd940ed6
Olga Grishchenko <104859965+OlgaGrishchenko@users.noreply.github.com>
2022-11-16 12:08:26 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,39 +14,15 @@ import ThemeProvider from "./Context/Theme/ThemeProvider";
 import { Theme } from "./Constants/@types"
 import ThemeSwitcher from "./Components/ThemeSwitcher";
 import Footer from "./Components/Footer";
-
-const MOCK_CARD = {
-    id: 0,
-    image: "https://pibig.info/uploads/posts/2022-03/1648204988_5-pibig-info-p-kvadratnaya-priroda-priroda-krasivo-foto-6.jpg",
-    text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
-    date: "2022-11-01",
-    lesson_num: 0,
-    title: "Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
-    author: 0,
-};
-
-const MOCK_CARDS_LIST = [
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-    MOCK_CARD,
-];
+import NewPassword from "./Pages/NewPassword";
+import Textarea from "./Components/TextArea/Textarea";
+import Router from "./Pages/Router";
+import ContentPage from "./Pages/ContentPage";
 
 const App = () => {
     const [cardsList, setCardsList] = useState<CardType[] | null>(null);
 
     const [theme, setTheme] = useState(Theme.Dark);
-
-    useEffect(() => {
-        setCardsList(MOCK_CARDS_LIST);
-    }, []);
 
     const onChangeTheme = (value: Theme) => {
         setTheme(value);
@@ -54,11 +30,7 @@ const App = () => {
 
     return (
         <ThemeProvider theme={theme} onChangeTheme={onChangeTheme}>
-            <div className={styles.container}>
-            <ThemeSwitcher />
-            <CardsList cardsList={MOCK_CARDS_LIST} />
-            <Footer />
-        </div>
+            <Router />
         </ThemeProvider>
         
     );
