c73490265fd4ab4909f69d3448f95e9fa41817a0
Olga Grishchenko <104859965+OlgaGrishchenko@users.noreply.github.com>
2022-12-14 18:56:20 +0300

    Update index.ts

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { BookmarkSelectIcon } from './icons/BookmarkSelectIcon';
-=======
->>>>>>> main
 export * from "./icons/BurgerClosedIcon";
 export * from "./icons/CloseIcon";
 export { LikeIcon } from "./icons/LikeIcon";
@@ -12,9 +9,5 @@ export { SunIcon } from "./icons/SunIcon";
 export { MoonIcon} from "./icons/MoonIcon";
 export { SearchIcon } from "./icons/SearchIcon";
 export { UserIcon } from "./icons/UserIcon";
-<<<<<<< HEAD
 export { CloseModalICon } from "./icons/CloseModalICon";
 export { BookmarkSelectIcon} from "./icons/BookmarkSelectIcon"
-=======
-export { CloseModalICon } from "./icons/CloseModalICon";
->>>>>>> main
