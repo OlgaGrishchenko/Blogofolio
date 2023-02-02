eee5126250cea6c8234e146ef8a46e0a3fa5f173
OlgaGrishchenko <olg.grishchenko@gmail.com>
2023-02-02 17:14:18 +0300

    fixed pagination

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -15,40 +15,33 @@ import { getMyPosts, getPosts } from "../../Redux/Reducers/postsReducer";
 import styles from "./Home.module.css";
 import AuthSelectors from "../../Redux/Selectors/authSelectors";
 import { PER_PAGE } from "../../Constants/consts";
-import rootSaga from "../../Redux/Sagas/rootSaga";
-import { RootState } from "../../Redux/store";
 import Loader from "../../Components/Loader";
 
 const Home = () => {
     const { theme } = useThemeContext();
-
-    const dispatch = useDispatch();
-    const allPosts = useSelector(PostsSelectors.getAllPosts);
-    const myPosts = useSelector(PostsSelectors.getMyPosts);
-    const [currentPage, setCurrentPage] = useState(1);
-    
-    useEffect(() => {
-        const offset = PER_PAGE * (currentPage - 1)
-        dispatch(getPosts({offset}));
-    }, [currentPage]);
-
 
     const [activeTab, setActiveTab] = useState(Tabs.All);
     const onTabClick = (tab: Tabs) => {
         setActiveTab(tab);
     };
 
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const dispatch = useDispatch();
+    const allPosts = useSelector(PostsSelectors.getAllPosts);
+    const myPosts = useSelector(PostsSelectors.getMyPosts);
+    
     const likedPosts = useSelector(PostsSelectors.getLikedPosts);
     const savedPosts = useSelector(PostsSelectors.getSavedPosts);
-    const totalCount = useSelector(PostsSelectors.getTotalCount);
+
+    const isLoggedIn = useSelector(AuthSelectors.getLoggedIn);
 
     const isLoading = useSelector(PostsSelectors.getPostsLoading);
 
+    const totalCount = useSelector(PostsSelectors.getTotalCount);
     const totalPagesCount = Math.ceil(totalCount/PER_PAGE);
-
-    const isLoggedIn = useSelector(AuthSelectors.getLoggedIn);
-    
     const pages = Array.from(Array(totalPagesCount).keys());
+    const onPageChange = (page: number) => () => setCurrentPage(page);
 
     const cardsArray = () => {
         if (activeTab === Tabs.Popular) {
@@ -62,7 +55,14 @@ const Home = () => {
         }
     };
 
-    const onPageChange = (page:number) => () => setCurrentPage(page);
+    useEffect(() => {
+        const offset = PER_PAGE * (currentPage - 1)
+        if (activeTab === Tabs.MyPosts) {
+            dispatch(getMyPosts());
+        } else {
+            dispatch(getPosts({ offset }));
+        }
+    }, [activeTab, currentPage]);
 
     const TABS_NAMES = useMemo ( () => [
         { name: "All", key: Tabs.All },
@@ -91,7 +91,9 @@ const Home = () => {
             <CardsList cardsList={cardsArray()} />
             <div className={styles.pagination}>
                 <div onClick={currentPage !==1 ? onPageChange(currentPage-1) : undefined}>← Prev</div>
-                <div className={styles.pageNumber}>{pages.map(i => (<div onClick={onPageChange(i+1)} key={i} className={classNames({[styles.activePage]:(i+1)===currentPage})}> {i+1} </div>))}</div>
+                <div className={styles.pageNumber}>
+                    {pages.map(i => (<div onClick={onPageChange(i+1)} key={i} className={classNames(styles.page, {[styles.activePage]: i + 1 === currentPage})}> {i+1} </div>))}
+                </div>
                 <div onClick={currentPage !==totalPagesCount ?onPageChange(currentPage+1) : undefined}>Next →</div>
             </div>
             <SelectedPostModal />
